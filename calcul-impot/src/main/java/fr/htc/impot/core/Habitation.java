244c30c4c35026c1e64725b853febe8ad7600301
Soraya Ouhachi <s.ouhachi@htcompass.fr>
2019-10-14 15:01:50 +0200

    Add Habitation Class

--- calcul-impot/src/main/java/fr/htc/impot/core/Habitation.java
+++ calcul-impot/src/main/java/fr/htc/impot/core/Habitation.java
@@ -1,19 +1,28 @@
 package fr.htc.impot.core;
 
 public class Habitation {
+	public String nom;
+	public String adresse;
+	public double surface;
 
-	public Habitation(String nom, String adresse, double suface) {
-		// TODO Auto-generated constructor stub
+	public Habitation(String nom, String adresse, double surface) {
+		this.nom = nom;
+		this.adresse = adresse;
+		this.surface = surface;
 	}
 
 	public double Impot() {
 		// TODO Auto-generated method stub
-		return 0;
+		Habitation hab = new Habitation(nom, adresse, surface);
+		double i = 2 * surface;
+		System.out.println("la somme d'impôt à payer est " + i);
+		return i;
 	}
 
 	public void Affiche() {
-		// TODO Auto-generated method stub
-		
+		System.out.println(nom);
+		System.out.println(adresse);
+		System.out.println(surface);
 	}
 
 }
