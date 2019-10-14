0d6d23302317d2f6cf7b910441a3985f2b633adb
Djamel MOUCHENE <dmouchene@gmail.com>
2019-10-14 16:11:49 +0200

    Add HabitationIndividuelle and override impot() & affiche()

--- calcul-impot/src/main/java/fr/htc/impot/utils/Constantes.java
+++ calcul-impot/src/main/java/fr/htc/impot/utils/Constantes.java
@@ -3,5 +3,7 @@ package fr.htc.impot.utils;
 public interface Constantes {
 	
 	public static final double PRIX_METRE_CARRE = 2;
+	public static final double PRIX_PAR_PIECE = 100;
+	public static final double PRIX_PAR_PISCINE = 500;
 
 }
